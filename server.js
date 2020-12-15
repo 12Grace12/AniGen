32ce93d379d33d0c769bc70a9a605d03c39b6efc
jessllrr <jessicaecheverria3@gmail.com>
2020-12-15 11:35:40 -0500

    setting up new server

--- server.js
+++ server.js
@@ -20,9 +20,14 @@ MongoClient.connect(mongo_uri, {
             console.log('Listening on 3000');
         })
 
+        app.get('/', function(req, res) {
+            res.sendFile(path.resolve("server-main.html"));
+        })
+
         app.post('/register', function(req, res) {
             // Create document including randomly generated key
-
+            console.log(req.body["username"])
+            res.send("<p>Key</p>")
             // Write key to page along with note
 
             // Add document to MongoDB
